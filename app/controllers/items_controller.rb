742a1d1348194091fc740d0698a9530e1c54cdd5
YUMASUZUKI0122 <aria122@icloud.com>
2021-01-21 12:30:31 +0900

    Revert "Revert "first commit""
    This reverts commit df525cb3acc16b1caf6e305800640f9a83246354.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:edit, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -10,7 +12,6 @@ class ItemsController < ApplicationController
   end
 
   def create 
-
    @item = Item.new(item_params)
    if @item.save
       redirect_to root_path
@@ -23,11 +24,33 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
 
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
   private 
   def item_params
   params.require(:item).permit(:name, :explanation, :category_id, :state_id, :burden_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to new_user_session_path
+    end
+   end
 
 end
