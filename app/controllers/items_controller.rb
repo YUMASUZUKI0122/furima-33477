20ca0d2a1f5e6bdf6db8894f1448f801dbca7fa6
YUMASUZUKI0122 <aria122@icloud.com>
2021-01-30 23:34:08 +0900

    login commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    if current_user == @item.user
+       redirect_to root_path
+      end
   end
 
   def update
