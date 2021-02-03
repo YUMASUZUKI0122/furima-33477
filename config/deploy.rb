2fb3df8ec40b490f996f4ec72ee31174e443b1b2
YUMASUZUKI0122 <aria122@icloud.com>
2021-02-03 19:10:28 +0900

    EC2　fifth commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.15.0"
 
 set :application, "furima-33477"
-set :repo_url, "git@example.com:YUMASUZUKI0122/furima-33477.git"
+set :repo_url, "git@github.com:YUMASUZUKI0122/furima-33477.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
